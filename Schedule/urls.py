c493a0ea2ed859a0f0cbdec0ff330ee1b92b58fb
Victor Nieves Sanchez <vnievess@gmail.com>
2019-01-25 16:56:30 +0100

    renamed project

--- Schedule/urls.py
+++ Schedule/urls.py
@@ -1,4 +1,4 @@
-"""horarios URL Configuration
+"""Schedule URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
