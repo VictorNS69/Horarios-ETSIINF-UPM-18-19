8648297480163056176dc909731b05f1f249bd4b
Victor Nieves Sanchez <vnievess@gmail.com>
2019-01-26 20:19:46 +0100

    Changed values for Type to match the languaje
    Changed to Spanish for the admin usage

--- ScheduleGenerator/models.py
+++ ScheduleGenerator/models.py
@@ -9,7 +9,7 @@ class Subject(models.Model):
     ects = models.PositiveSmallIntegerField(
         default=0, validators=[MinValueValidator(1)])
     TYPES = (
-        ("Basic", "Basic"), ("Obligatory", "Obligatory"), ("Optional", "Optional")
+        ("Básica", "Básica"), ("Obligatoria", "Obligatoria"), ("Opcional", "Opcional")
     )
     type = models.CharField(choices=TYPES, blank=False, max_length=10)
     SEMESTERS = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8))
