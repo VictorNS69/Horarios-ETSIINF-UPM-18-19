207fc618c938f2d2033fb5d2a82d9f95252b8ec9
Victor Nieves Sanchez <vnievess@gmail.com>
2019-01-28 13:58:05 +0100

    edited the DataObject class

--- ImportData/DataObject.py
+++ ImportData/DataObject.py
@@ -1,31 +1,43 @@
-class DataObject:
-    def __init__(self):
-        self.code = None
-        self.name = None
-        self.type = None
-        self.ects = None
-        self.schedules = {}
+import numpy as np
 
-    def make_schedules(self, all_groups):
-        all_groups = all_groups.replace("\n", "")
-        all_groups = all_groups.replace("\r", "")
-        groups = all_groups.split("@")
-        for i in groups:
-            group_sche = i.split(":")
-            self.schedules[group_sche[0]] = group_sche[1]
 
-    def make_attributes(self):
-        with open("data.dat") as f:
-            for line in f:
-                splitter = line.split("@", 4)
-        self.code = splitter[0]
-        self.name = splitter[1]
-        self.type = splitter[2]
-        self.ects = splitter[3]
-        self.make_schedules(splitter[4])
+class Subject:
+    def __init__(self, code, name, type, ects, schedules):
+        self.code = code
+        self.name = name
+        self.type = type
+        self.ects = ects
+        self.schedules = schedules
+
+
+def make_schedules(all_groups):
+    schedules = {}
+    all_groups = all_groups.replace("\n", "")
+    all_groups = all_groups.replace("\r", "")
+    groups = all_groups.split("@")
+    for i in groups:
+        group_sche = i.split(":")
+        schedules[group_sche[0]] = group_sche[1]
+    return schedules
+
+
+def make_attributes():
+    subjects = []
+    with open("data.dat") as f:
+        for line in f:
+            splitter = line.split("@", 4)
+            code = splitter[0]
+            name = splitter[1]
+            type = splitter[2]
+            ects = splitter[3]
+            schedules = make_schedules(splitter[4])
+            subjects.append(Subject(code, name, type, ects, schedules))
+    return subjects
+
 
 '''
 if __name__ == "__main__":
-    do = DataObject()
-    do.make_attributes()
-    print(do.name)'''
+    subjects = make_attributes()
+    for item in subjects:
+        print (item.name)
+'''
