1c33e84be675b1ccc29e8851ba3ecfeadbf8ffd3
Victor Nieves Sanchez <vnievess@gmail.com>
2019-02-01 21:42:14 +0100

    added filter and order to the admin page

--- ScheduleGenerator/admin.py
+++ ScheduleGenerator/admin.py
@@ -5,6 +5,7 @@ from .models import Subject
 class SubjectAdmin(admin.ModelAdmin):
     list_display = ["code", "name", "type", "semester", "ects"]
     ordering = ["code"]
+    list_filter = ("type", "semester", "ects")
 
 
 admin.site.register(Subject, SubjectAdmin)
